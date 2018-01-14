c5e7397d75e29f5f959c9dcbe6eade85fce4d04c
rebirthtobi <ttobi4@gmail.com>
2018-01-14 05:24:29 -0800

    changing configuration

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 const config = {
     MEETUP_API_KEY: '4e2e232644512452758583413771259',
     EVENTBRITE_TOKEN: 'TL4L2FOKRUKU7CLIJTS2',
-    LOGIN_URL: 'http://akojopo-api.herokuapp.com/api/v1/login',
-    RENEW_URL: 'http://akojopo-api.herokuapp.com/api/v1/renew',
+    LOGIN_URL: 'https://akojopo-api.herokuapp.com/api/v1/login',
+    RENEW_URL: 'https://akojopo-api.herokuapp.com/api/v1/renew',
     REGISTRATION_URL: 'http://akojopo-api.heroku.com/api/v1/register',
     ENCRYPTION_KEY: 'qzWLTr/JRsbiARfB/WPnq+du2dyj/p71P/TkoXJ5//s=',
     AUTH_SECRET_KEY: 'r5y4nVUt3L5rqjEcppqa+JUPT276MZo3pT14asSTsVA='
